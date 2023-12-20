6508dc4fb178a3eb0d26fd8c2ad31dd4eec64246
Maythawat Mahawan <moking55@hotmail.com>
2023-12-20 14:17:38 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-layout>
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-layout>
+    
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    Hello world
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeView"
+}
+</script>
+
+<style scoped>
+.section-header {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    overflow: hidden;
+    line-height: 0;
+    transform: rotate(180deg);
+}
+
+.section-header svg {
+    position: relative;
+    display: block;
+    width: calc(100% + 1.3px);
+    height: 150px;
+}
+
+.section-header .shape-fill {
+    fill: #FFFFFF;
+}
+</style>
